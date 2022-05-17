2cbd49108ac130c8da5ee595cf6a9246d3e02032
Kelly Woldseth <70242352+kwoldseth@users.noreply.github.com>
2022-05-17 12:11:37 -0700

    Add files via upload

---
+++ FivePlanetTravel.java
@@ -0,0 +1,34 @@
+/* 
+ * Activity 1.1.5
+ */
+public class FivePlanetTravel
+{
+  public static void main(String[] args)
+  {
+    // theplanets.org average distance from earth to the planets
+    int mercury = 56974146;
+    int venus = 25724767;
+    int mars = 48678219;
+    int jupiter = 390674710;
+    int saturn = 792248270;
+
+    // number of planets to visit
+    int numPlanets = 5;
+
+    // speed of light and our speed
+    int lightSpeed =  670616629;
+    lightSpeed /= 10;
+
+    // total travel time
+    double total = 0;
+
+    //widening (Step 15)
+
+    //casting (Step 15)
+
+    //average (Steps 19-21)
+    
+    
+    
+  }
+}
---
+++ PlanetTravel.java
@@ -0,0 +1,15 @@
+/* 
+ * Activity 1.1.5
+ */
+public class PlanetTravel
+{
+  public static void main(String[] args)
+  {
+    // the planet's average distance from earth 
+    int mars = 48678219;
+    // fastest spacecraft in mph
+    int fastest = 24791;
+
+    System.out.println("Travel time to Mars: " + mars/fastest + " hours.");
+  }
+}
